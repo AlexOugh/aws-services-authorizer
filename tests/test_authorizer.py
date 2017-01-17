df947ad0775ed653f691924fd506163af2cc8048
Alex Ough <alex.ough@sungardas.com>
2017-01-17 16:37:42 -0600

    use local file/folder in template and config.json for input params

--- tests/test_authorizer.py
+++ tests/test_authorizer.py
@@ -18,6 +18,6 @@ from authorizer import lambda_handler
 context = None
 event = {
     'authorizationToken': refresh_token,
-    'methodArn': 'arn:aws:lambda:us-east-1:089476987273:function/aaa:custom_authorizer'
+    'methodArn': 'arn:aws:lambda:us-east-1:11111111:function/aaa:custom_authorizer'
 }
 lambda_handler(event, context)
